2d7404f82c8042b507df69a1d5c87f39155e472f
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-05-22 17:32:00 +0300

    Fix support of - and + prefix operators in JS parser
    Treat number literal that represents minimum integer as integer node in
    JS AST.
    
    Treat +<number_literal> JS AST as unary node (+) containing single
    number literal

--- js/js.parser/src/com/google/gwt/dev/js/rhino/Parser.java
+++ js/js.parser/src/com/google/gwt/dev/js/rhino/Parser.java
@@ -1053,8 +1053,7 @@ public class Parser extends Observable {
                                 property = nf.createString(ts.getString(), ts.tokenPosition);
                                 break;
                             case TokenStream.NUMBER_INT:
-                                int n = (int) ts.getNumber();
-                                property = nf.createNumber(n, ts.tokenPosition);
+                                property = nf.createIntNumber(ts.getNumber(), ts.tokenPosition);
                                 break;
                             case TokenStream.NUMBER:
                                 double d = ts.getNumber();
@@ -1099,8 +1098,7 @@ public class Parser extends Observable {
                 return nf.createName(name, position);
 
             case TokenStream.NUMBER_INT:
-                int n = (int) ts.getNumber();
-                return nf.createNumber(n, position);
+                return nf.createIntNumber(ts.getNumber(), position);
 
             case TokenStream.NUMBER:
                 double d = ts.getNumber();
